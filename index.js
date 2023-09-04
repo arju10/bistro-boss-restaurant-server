108811427c4e4a9ac28be0875d7ca7f1765a04b9
arju10 <arjuarju71@gmail.com>
2023-09-04 16:52:14 +0600

    created cart get api

--- index.js
+++ index.js
@@ -39,14 +39,18 @@ async function run() {
       res.send(result);
     })
 
-    // cart collection
-    app.post('/carts', async (req, res) => {
-      const item = req.body;
-      console.log(item);
-      const result = await cartCollection.insertOne(item);
-      res.send(result);
-    })
+    // cart collection apis
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
 
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

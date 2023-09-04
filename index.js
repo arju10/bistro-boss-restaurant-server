f330e0202c4f099bc34a46077c01a2a47c768994
arju10 <arjuarju71@gmail.com>
2023-09-04 16:59:35 +0600

    resolve created cart api -> Post Cart

--- index.js
+++ index.js
@@ -50,12 +50,21 @@ async function run() {
       res.send(result);
     });
 
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+    
     app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
